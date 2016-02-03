3534fe75139b88ca7452e0ff8379683793e96fe8
Ozzerov <ao@wheely.com>
2016-02-03 17:58:39 +0300

    no message

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='pd2gs',
       version='0.1',
       description='Simple tool to export pandas dataframes to google sheets.',
+      url='https://github.com/Ozzerov/pd2gs',
       author='Alex Ozerov',
       author_email='ozerov@yandex.ru',
       license='MIT',
