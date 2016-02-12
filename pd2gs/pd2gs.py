b8aae8b6ffeac45dec41eb25e90d00983b9ada1a
Ozzerov <ao@wheely.com>
2016-02-12 14:00:02 +0300

    no message

--- pd2gs/pd2gs.py
+++ pd2gs/pd2gs.py
@@ -37,12 +37,14 @@ class ConnectGoogleSheet:
         _ws.update_cells(cell_list)
 
     def write(self, df, worksheet='Sheet1', start_from='A1'):
+        df = df.reset_index()
         """
         :param df: pandas DataFrame
         :param worksheet: worksheet name
         :param start_from: cell to use for top left corner of a DataFrame
         """
         n_rows, n_cols = df.shape
+        n_cols -= 1
         start_col, start_row = re.findall('\\d+|\\D+', start_from)
         start_col = sum([(ord(l) - 64) * 26 ** n for (l, n) in zip(start_col.upper(),
                                                                    range(len(start_col) - 1, -1, -1))])
@@ -54,13 +56,10 @@ class ConnectGoogleSheet:
                                           rows=max(start_row + n_rows + 1, 26),
                                           cols=max(start_col + n_cols + 1, 26))
 
-        index_range = '{0}{1}:{2}{3}'.format(self._num2letters(start_col), start_row + 1,
-                                             self._num2letters(start_col), start_row + n_rows)
-        columns_range = '{0}{1}:{2}{3}'.format(self._num2letters(start_col + 1), start_row,
+        columns_range = '{0}{1}:{2}{3}'.format(self._num2letters(start_col), start_row,
                                                self._num2letters(start_col + n_cols), start_row)
-        values_range = '{0}{1}:{2}{3}'.format(self._num2letters(start_col + 1), start_row + 1,
+        values_range = '{0}{1}:{2}{3}'.format(self._num2letters(start_col), start_row + 1,
                                               self._num2letters(start_col + n_cols), start_row + n_rows)
 
-        self._update_cells(ws, index_range, df.index)
         self._update_cells(ws, columns_range, df.columns)
         self._update_cells(ws, values_range, df.values.ravel())
